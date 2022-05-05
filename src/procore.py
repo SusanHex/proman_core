c1c4e8873425f1522441cb75f3c758ee76972938
SusanHex <suehex4027@gmail.com>
2022-05-05 01:09:58 -0600

    Finally got a working test

--- src/procore.py
+++ src/procore.py
@@ -1,42 +1,70 @@
 import asyncio
-from concurrent.futures import process
 import logging
-from asyncio import create_subprocess_exec, create_subprocess_shell, create_task
-from asyncio.subprocess import Process, PIPE
-from asyncio.queues import Queue, QueueFull
-from multiprocessing.connection import wait
+import os
 
-logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+logging.basicConfig(level=logging.INFO, format='%(message)s')
 logger = logging.getLogger(__name__)
 class Manager(object):
-    def __init__(self, cmd) -> None:
+    def __init__(self, cmd, manage_stdin: bool = False, dedicated_stderr: bool = False) -> None:
         self._cmd = cmd
-
+        self._manage_stdin = manage_stdin
+        self._dedicated_stderr = dedicated_stderr
+        
     async def start(self) -> None:
-        self._process = await create_subprocess_shell(
-            self._cmd, stdin=PIPE, stderr=PIPE, stdout=PIPE
-        )
+        if self._dedicated_stderr and self._manage_stdin:
+            self._process = await asyncio.subprocess.create_subprocess_shell(
+                self._cmd, stderr=asyncio.subprocess.PIPE, stdout=asyncio.subprocess.PIPE, stdin=asyncio.subprocess.PIPE
+            )
+        elif self._dedicated_stderr:
+            self._process = await asyncio.subprocess.create_subprocess_shell(
+                self._cmd, stderr=asyncio.subprocess.PIPE, stdout=asyncio.subprocess.PIPE
+            )
+        elif self._manage_stdin:
+            self._process = await asyncio.subprocess.create_subprocess_shell(
+                self._cmd, stderr=asyncio.subprocess.STDOUT, stdout=asyncio.subprocess.PIPE, stdin=asyncio.subprocess.PIPE
+            )
+        else:
+            self._process = await asyncio.subprocess.create_subprocess_shell(
+                self._cmd, stderr=asyncio.subprocess.STDOUT, stdout=asyncio.subprocess.PIPE
+            )
+            
         logger.info(f'Created the process: "{self._process}"')
         # Create the queues
-        self._output_queue = Queue()
-        self._error_queue = Queue()
-        self._input_queue = Queue()
+        self._output_queue = asyncio.Queue()
+        self._error_queue = asyncio.Queue()
+        self._input_queue = asyncio.Queue()
         logger.info("Created the queues")
-        # create the tasks
-        self._output_task = await create_task(
+        #create the tasks
+        
+        self._output_task = asyncio.create_task(
             Manager.output_runner(self._process, self._output_queue)
         )
-        self._error_task = await create_task(
-            Manager.error_runner(self._process, self._error_queue)
-        )
-        self._input_queue = await create_task(
-            Manager.input_runner(self._process, self._input_queue)
-        )
-        logger.info("Created the tasks")
+        if self._dedicated_stderr:
+            self._error_task = asyncio.create_task(
+                Manager.error_runner(self._process, self._error_queue)
+            )
+        if self._manage_stdin:
+            self._input_task = asyncio.create_task(
+                Manager.input_runner(self._process, self._input_queue)
+            )
+        logger.info("Created the task(s)")
+    
+    async def read(self):
+        try:
+            return await self._output_queue.get()
+        except asyncio.QueueEmpty:
+            return b''
+    
+    async def write(self, data: bytes = b'\n'):        
+        if self._process.returncode is None:
+            if data.endswith(b'\n'):
+                self._input_queue.put(data)
+            else:
+                self._input_queue.put(data + b'\n')
 
     @staticmethod
-    async def output_runner(proc: Process, queue: Queue):
-        logger.info("Output runner has started")
+    async def output_runner(proc: asyncio.subprocess.Process, queue: asyncio.Queue):
+        logger.debug("Output runner has started")
         while proc.returncode is None:
             data = await proc.stdout.readline()
             if not data:
@@ -44,45 +72,50 @@ class Manager(object):
             logger.debug(f'Output runner: "{data}", {proc.returncode} {bool(data)}')
             try:
                 await queue.put(data)
-            except QueueFull:
+            except asyncio.QueueFull:
                 logger.warning(f'Output queue is full, removing "{await queue.get()}"')
                 await queue.put(data)
-        logger.info("Output runner has finished")
+            await asyncio.sleep(0)
+        logger.debug("Output runner has finished")
 
     @staticmethod
-    async def error_runner(proc: Process, queue: Queue):
+    async def error_runner(proc: asyncio.subprocess.Process, queue: asyncio.Queue):
         logger.info("Error runner has started")
         while proc.returncode is None:
             data = await proc.stdout.readline()
+            if not data:
+                break
             logger.debug(f'Error runner: "{data}"')
-            
+
             try:
                 await queue.put(data)
-            except QueueFull:
+            except asyncio.QueueFull:
                 logger.warning(f'Error queue is full, removing "{await queue.get()}"')
                 await queue.put(data)
         logger.info("Error runner has finished")
 
     @staticmethod
-    async def input_runner(proc: Process, queue: Queue):
-        logger.info("Info runner has started")
-        while Process.returncode is None:
+    async def input_runner(proc: asyncio.subprocess.Process, queue: asyncio.Queue, delay: float = 0):
+        logger.info("Input runner has started")
+        while proc.returncode is None:
             if queue.qsize() > 0:
                 data = await queue.get()
-                logger.debug(f'Input runner: "{data}"')
-                if not data.endswith("\n"):
-                    data = data + "\n"
-                logger.debug("Info runner wrote data to process")
+                if proc.stdin.is_closing():
+                    break
+                else:
+                    proc.stdin.write(data)
+                    await proc.stdin.drain()
+            else:
+                await asyncio.sleep(0)
         logger.info("Input runner has finished")
 
 
 if __name__ == "__main__":
-    logger.setLevel(logging.DEBUG)
     async def main():
-        logger = logging.Logger("root")
-        logger.setLevel(logging.DEBUG)
-        proc = Manager('python -c \"print(\'hello world\')\"')
+        proc = Manager(r"echo hello && pause")
         await proc.start()
-        print(await proc._output_queue.get())
+        while proc._process.returncode is None:
+            print((await proc.read()).decode(), end='')
+
 
     asyncio.run(main())
