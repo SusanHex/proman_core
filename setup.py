cdd27c596a0051b05edcaa8882b13e1b082ca089
SusanHex <suehex4027@gmail.com>
2022-05-06 19:41:47 -0600

    setup file for the eventual package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='promanager',
+    version='2022.5.5',
+    description='A package to manage child processes',
+    long_description='A package to manage child processes',
+    author='Susan Hex',
+)
