c33e6c70b7e548ddce3ed71debabe9a8b7eee3cc
SusanHex <suehex4027@gmail.com>
2022-05-10 22:33:16 -0600

    Switched to black

--- src/procore.py
+++ src/procore.py
@@ -5,47 +5,60 @@ import aiohttp
 import logging
 from re import match, sub
 
-logging.basicConfig(level=logging.INFO, format='%(message)s')
+logging.basicConfig(level=logging.INFO, format="%(message)s")
 logger = logging.getLogger(__name__)
-    
+
+
 class Manager(object):
-    def __init__(self, cmd, manage_stdin: bool = False, dedicated_stderr: bool = False) -> None:
+    def __init__(
+        self, cmd, manage_stdin: bool = False, dedicated_stderr: bool = False
+    ) -> None:
         self._cmd = cmd
         self._manage_stdin = manage_stdin
         self._dedicated_stderr = dedicated_stderr
         self._process = None
-    
+
     def __del__(self):
         if not self._process is None:
-            logger.debug('Killing the process')
+            logger.debug("Killing the process")
             self._process.kill()
-        
+
     async def start(self) -> None:
         if self._dedicated_stderr and self._manage_stdin:
             self._process = await asyncio.subprocess.create_subprocess_shell(
-                self._cmd, stderr=asyncio.subprocess.PIPE, stdout=asyncio.subprocess.PIPE, stdin=asyncio.subprocess.PIPE
+                self._cmd,
+                stderr=asyncio.subprocess.PIPE,
+                stdout=asyncio.subprocess.PIPE,
+                stdin=asyncio.subprocess.PIPE,
             )
         elif self._dedicated_stderr:
             self._process = await asyncio.subprocess.create_subprocess_shell(
-                self._cmd, stderr=asyncio.subprocess.PIPE, stdout=asyncio.subprocess.PIPE
+                self._cmd,
+                stderr=asyncio.subprocess.PIPE,
+                stdout=asyncio.subprocess.PIPE,
             )
         elif self._manage_stdin:
             self._process = await asyncio.subprocess.create_subprocess_shell(
-                self._cmd, stderr=asyncio.subprocess.STDOUT, stdout=asyncio.subprocess.PIPE, stdin=asyncio.subprocess.PIPE
+                self._cmd,
+                stderr=asyncio.subprocess.STDOUT,
+                stdout=asyncio.subprocess.PIPE,
+                stdin=asyncio.subprocess.PIPE,
             )
         else:
             self._process = await asyncio.subprocess.create_subprocess_shell(
-                self._cmd, stderr=asyncio.subprocess.STDOUT, stdout=asyncio.subprocess.PIPE
+                self._cmd,
+                stderr=asyncio.subprocess.STDOUT,
+                stdout=asyncio.subprocess.PIPE,
             )
-            
+
         logger.info(f'Created the process: "{self._process}"')
         # Create the queues
         self._output_queue = asyncio.Queue()
         self._error_queue = asyncio.Queue()
         self._input_queue = asyncio.Queue()
         logger.info("Created the queues")
-        #create the tasks
-        
+        # create the tasks
+
         self._output_task = asyncio.create_task(
             Manager.output_runner(self._process, self._output_queue)
         )
@@ -58,19 +71,19 @@ class Manager(object):
                 Manager.input_runner(self._process, self._input_queue)
             )
         logger.info("Created the task(s)")
-    
+
     async def read(self):
         try:
             return await self._output_queue.get()
         except asyncio.QueueEmpty:
-            return b''
-    
-    async def write(self, data: bytes = b'\r\n'):        
+            return b""
+
+    async def write(self, data: bytes = b"\r\n"):
         if self._process.returncode is None:
-            if data.endswith(b'\r\n'):
+            if data.endswith(b"\r\n"):
                 self._input_queue.put(data)
             else:
-                self._input_queue.put(data + b'\r\n')
+                self._input_queue.put(data + b"\r\n")
 
     @staticmethod
     async def output_runner(proc: asyncio.subprocess.Process, queue: asyncio.Queue):
@@ -105,7 +118,9 @@ class Manager(object):
         logger.info("Error runner has finished")
 
     @staticmethod
-    async def input_runner(proc: asyncio.subprocess.Process, queue: asyncio.Queue, delay: float = 0):
+    async def input_runner(
+        proc: asyncio.subprocess.Process, queue: asyncio.Queue, delay: float = 0
+    ):
         logger.info("Input runner has started")
         while proc.returncode is None:
             if queue.qsize() > 0:
@@ -119,57 +134,66 @@ class Manager(object):
                 await asyncio.sleep(0)
         logger.info("Input runner has finished")
 
+
 async def load_config(config_path: str) -> dict:
-    with open(config_path, 'r') as config_file:
-        if config_path.endswith('.yml'):
+    with open(config_path, "r") as config_file:
+        if config_path.endswith(".yml"):
             return yaml.safe_load(config_file)
-        elif config_path.endswith('.json'):
+        elif config_path.endswith(".json"):
             return json.load(config_file)
         else:
-            raise ValueError(f'"{config_file}" is not a supported config file type. (*.json, *.yml)')
-        
-async def perform_action(action: dict = {}, data: str = '') -> None:
+            raise ValueError(
+                f'"{config_file}" is not a supported config file type. (*.json, *.yml)'
+            )
+
+
+async def perform_action(action: dict = {}, data: str = "") -> None:
     logger.debug(f"{__name__} is starting")
-    if 'condition' in action.keys() and match(action['condition'], data):
-        logger.debug('action condition matches data')
-        if 'remove_steps' in action.keys():
+    if "condition" in action.keys() and match(action["condition"], data):
+        logger.debug("action condition matches data")
+        if "remove_steps" in action.keys():
             logger.debug(f"Found remove_steps: {len(action['remove_steps'])} steps")
-            for remove_step in action['remove_steps']:
+            for remove_step in action["remove_steps"]:
                 # pass the execution to the loop
                 asyncio.sleep(0)
                 # if the remove step matches, remove the match.
                 pre_data = data
-                data = sub(remove_step, '', data)
+                data = sub(remove_step, "", data)
                 # helps debug steps
                 if data == pre_data:
                     logger.warning(f'"{remove_step}" had no effect')
-        if 'web' in action.keys():
+        if "web" in action.keys():
             session = aiohttp.ClientSession()
-            await web_action(session=session, scheme=action['web'], data=data)
+            await web_action(session=session, scheme=action["web"], data=data)
 
-async def web_action(session: aiohttp.ClientSession, scheme: dict = {}, data: str = '') -> None:
-    url = scheme['url']
-    method = scheme['method'] if scheme.get('method') else 'POST'
-    body = json.dumps(scheme['body'])
-    if '<data>' in body:
-        body = body.replace('<data>', data)
-        logger.debug('Replaced data in body')
+
+async def web_action(
+    session: aiohttp.ClientSession, scheme: dict = {}, data: str = ""
+) -> None:
+    url = scheme["url"]
+    method = scheme["method"] if scheme.get("method") else "POST"
+    body = json.dumps(scheme["body"])
+    if "<data>" in body:
+        body = body.replace("<data>", data)
+        logger.debug("Replaced data in body")
     async with session.request(method=method, url=url, data=body) as req:
         logger.debug(req)
 
+
 if __name__ == "__main__":
+
     async def main():
         logger.setLevel(logging.DEBUG)
         from sys import argv
+
         config = await load_config(argv[1])
-        proc = Manager(config['command'])
+        proc = Manager(config["command"])
         await proc.start()
         while proc._process.returncode is None:
             data = (await proc.read()).decode()
             # await perform_action(action=config['action'], data=data)
-            print(data, end='')
-        logger.debug('Main function has stopped the loop')
+            print(data, end="")
+        logger.debug("Main function has stopped the loop")
         asyncio.get_running_loop().close()
-
 
     asyncio.run(main())
